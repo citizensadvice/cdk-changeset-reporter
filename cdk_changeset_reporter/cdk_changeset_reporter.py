800a60d9363718f4ad7f76b574b41eef02ff42e1
Matthew Cane <matthew.cane@citizensadvice.org.uk>
2023-07-25 15:57:34 +0100

    Add emoji back in

--- cdk_changeset_reporter/cdk_changeset_reporter.py
+++ cdk_changeset_reporter/cdk_changeset_reporter.py
@@ -144,7 +144,7 @@ class CdkChangesetReporter:
                 # and add a warning to the change reason
                 recreate = True
                 requires_recreate = (
-                    f"### $\\textcolor{{red}}{{\\textsf{{{requires_recreate}}}}}$"
+                    f"🚨{requires_recreate}🚨"
                 )
 
             # Add the formatted details to the list of changes
